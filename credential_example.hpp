e459961941c3dc4d3766bbc7dc164535567e2a8c
Tom Schwanke <tomschwanke80@gmail.com>
2021-01-30 11:37:24 +0100

    Delete credential_example.hpp

--- credential_example.hpp
+++
@@ -1,9 +0,0 @@
-// OTA password (md5)
-const char* otapasshash     = "";
-
-// Config mode config
-char* configSsid            = "WiFiWeatherDisplay Setup";
-char* configPass            = "12345678";
-
-// OpenWeatherMap API key
-const char* apikey          = "";
