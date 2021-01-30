2db81afbd9eea8f4d7c179eb118f8c4dae7286f3
Tom Schwanke <tomschwanke80@gmail.com>
2021-01-30 11:38:36 +0100

    Create credentials.hpp
    This is an example for the credentials file and will need to be filed out with your API-Key

---
+++ credentials.hpp
@@ -0,0 +1,9 @@
+// OTA password (md5)
+const char* otapasshash     = "";
+
+// Config mode config
+char* configSsid            = "WiFiWeatherDisplay Setup";
+char* configPass            = "12345678";
+
+// OpenWeatherMap API key
+const char* apikey          = "";
