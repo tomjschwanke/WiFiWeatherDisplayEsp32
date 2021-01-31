2e51868d712ce9d63d430f03939cb80bde95c015
Tom Jason Schwanke <tomschwanke80@gmail.com>
2021-01-31 15:14:55 +0100

    Change char arrays defined in credential example file

--- credential_example.hpp
+++ credential_example.hpp
@@ -2,8 +2,8 @@
 const char* otapasshash     = "";
 
 // Config mode config
-char* configSsid            = "WiFiWeatherDisplay Setup";
-char* configPass            = "12345678";
+const char* configSsid            = "WiFiWeatherDisplay Setup";
+const char* configPass            = "12345678";
 
 // OpenWeatherMap API key
 const char* apikey          = "";
